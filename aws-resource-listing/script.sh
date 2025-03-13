61b6b8df32f68d16cad4804054b93666631fd4b5
haren035 <haren3035@gmail.com>
2025-03-14 10:00:05 +1100

    Add AWS service case handling for EC2 & S3

--- aws-resource-listing/script.sh
+++ aws-resource-listing/script.sh
@@ -26,3 +26,36 @@ if [ $# -ne 2 ]; then
     exit 1
 fi
 
+#check if awscli installed
+ if [ ! aws --version &> /dev/null ]; then
+          echo "awscli is not installed"
+          exit 1
+fi
+
+echo "cli is installed"
+
+
+
+#check if awscli is configured
+if [ ! ~/.aws ]; then
+          echo "awscli is not comfigured"
+          exit 1
+fi
+echo "cli is configured"
+
+#codes to check aws resources
+#check for ec2
+
+aws_service=$1
+aws_region=$2
+case $aws_service in
+          ec2)      #lists EC2 instances
+                    echo "Listing EC2 Instances in $aws_region"
+                    aws ec2 describe-instances --region aws_region
+                    ;;
+          
+          s3)       #Lists buckets
+                    echo "Listing S3 Buckets in $aws_region"
+                    aws s3api list-buckets --region aws_region
+                    ;;
+esac
