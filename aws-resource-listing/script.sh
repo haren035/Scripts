0d9db1c7b756088bf87f36caa7212dae64c3ed59
haren035 <haren3035@gmail.com>
2025-03-13 13:53:35 +1100

    script description done

---
+++ aws-resource-listing/script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+####################################################################################################
+
+#Author: Haren Sah
+#Version: v0.0.1
+
+# Script to automate the process of listing all the resources in an aws account
+#
+# Below are the services that are supported by this script(may add more services later)
+# 1. EC2
+# 2. S3
+#
+# The script will prompt the user to enter AWS region and the service for which need to be listed
+#
+#
+# Usage: ./aws_resource_list.sh <aws_region> <aws_service>
+# Example: ./aws_resource_list.sh ap-southeast-2 ec2
+
+####################################################################################################
