99ee31d7335ee93f1e46c0d84c7984e74f392193
haren035 <haren3035@gmail.com>
2025-03-18 10:58:25 +1100

    check root priviledge and path file

---
+++ Create Users/create_users.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+####################################################################################################
+# Author: Haren Sah
+# Version: v0.0.1
+#
+#
+#
+#
+#
+#
+####################################################################################################
+
+# Define the file paths for the logfile, and the password file
+INPUT_FILE="$1"
+LOG_FILE="/var/log/user_management.log"
+PASSWORD_FILE="/var/secure/user_passwords.csv"
+
+# Ensure script is run with root privileges
+if [ "$ID" -ne 0 ]; then
+          echo "Please run this script as root or sudo privileges "
+          exit 1
+fi
+
+# Check if user list file path is provided as argument
+if [ ""$#"" -ne 1 ]; then
+          echo "Usage: $0 <input_file>"
+          exit 1
+fi
