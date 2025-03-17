59af1c13c7451429d7dcb676df6012db248153e9
haren035 <haren3035@gmail.com>
2025-03-18 10:59:59 +1100

    syntax issues fix

--- Create Users/create_users.sh
+++ Create Users/create_users.sh
@@ -17,13 +17,13 @@ LOG_FILE="/var/log/user_management.log"
 PASSWORD_FILE="/var/secure/user_passwords.csv"
 
 # Ensure script is run with root privileges
-if [ "$ID" -ne 0 ]; then
+if [ "$EUID" -ne 0 ]; then
           echo "Please run this script as root or sudo privileges "
           exit 1
 fi
 
 # Check if user list file path is provided as argument
-if [ ""$#"" -ne 1 ]; then
+if [ $# -ne 1 ]; then
           echo "Usage: $0 <input_file>"
           exit 1
 fi
